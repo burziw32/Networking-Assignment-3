82490c498f57a73679066220ec025708d941ee42
burziw32 <Burziw32@uwosh.edu>
2013-11-18 18:58:09 -0600

    Update peer.py

--- peer.py
+++ peer.py
@@ -62,7 +62,7 @@ pathToDirectory = str(sys.argv[4])
 peerIP = str(sys.argv[5])
 peerPort = str(sys.argv[6])
 
-if (len(sys.argv) < 4 or len(sys.argv) = 5 or len(sys.argv) > 6):
+if (len(sys.argv) < 4 or len(sys.argv) == 5 or len(sys.argv) > 6):
     print "incorrect number of argv"
 elif (len(sys.argv) == 4):
     print "First peer"
