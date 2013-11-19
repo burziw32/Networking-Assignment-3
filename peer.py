fa6bb678ef86623974748f323295a6ae1a32cc29
nfg <nfg@nfgs-MacBook-Pro.local>
2013-11-18 19:01:50 -0600

    walter likes dick

--- peer.py
+++ peer.py
@@ -55,21 +55,23 @@ class TCPServer (threading.Thread):
         print "Exiting TCP server thread"
 
         
+if (len(sys.argv) < 4 or len(sys.argv) == 5 or len(sys.argv) > 6):
+    print "incorrect number of argv"
+    sys.exit("Error message")
+elif (len(sys.argv) == 4):
+    print "First peer"
+elif (len(sys.argv) == 6):
+    print "Peer"
+else:
+    print "Error"
+    sys.exit("Error message")
+
 peername = str(sys.argv[1])
 myIP = str(sys.argv[2])
 myPort = str(sys.argv[3])
 pathToDirectory = str(sys.argv[4])
 peerIP = str(sys.argv[5])
 peerPort = str(sys.argv[6])
-
-if (len(sys.argv) < 4 or len(sys.argv) == 5 or len(sys.argv) > 6):
-    print "incorrect number of argv"
-elif (len(sys.argv) == 4):
-    print "First peer"
-elif (len(sys.argv)== 6):
-    print "Peer"
-else:
-    print "Error"
 
 
 def runTCPClient(str,tcp):
