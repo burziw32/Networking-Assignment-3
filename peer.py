c269f3639b177dd47b13dc94842ca8d890f1836a
nfg <nfg@nfgs-MacBook-Pro.local>
2013-11-18 18:53:40 -0600

    add threads

--- peer.py
+++ peer.py
@@ -6,6 +6,53 @@ Walter Burzik
 '''
 import os, sys
 from socket import *
+import threading
+
+class UDPServer (threading.Thread):
+    def __init__(self, threadID,serverPort):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.serverPort = serverPort
+        self.counter = 0
+        self.done = False
+        
+    def run(self):
+        print "Starting UDP server thread"
+	serverSocket = socket(AF_INET,SOCK_DGRAM)
+	serverSocket.bind(('',self.serverPort))
+	print "The UDP server is ready to receive"
+	while not(self.done):
+		mesg, clientAddress = serverSocket.recvfrom(2048)
+		self.counter +=1
+		modifiedMesg = mesg.upper()
+		serverSocket.sendto(modifiedMesg,clientAddress)
+
+        print "Exiting UDP server thread"
+
+
+class TCPServer (threading.Thread):
+    def __init__(self, threadID,serverPort):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.serverPort = serverPort
+        self.counter = 0
+        self.done = False
+        
+    def run(self):
+        print "Starting TCP server thread"
+	serverSocket = socket(AF_INET,SOCK_STREAM)
+	serverSocket.bind(('',self.serverPort))
+	serverSocket.listen(5)
+	print "The TCP server is ready to receive"
+	while not(self.done):
+		connectionSocket,addr = serverSocket.accept()
+		self.counter +=1
+		mesg= connectionSocket.recv(2048)
+		modifiedMesg = mesg[::-1]
+		connectionSocket.send(modifiedMesg)
+		connectionSocket.close()        
+
+        print "Exiting TCP server thread"
 
         
 peername = str(sys.argv[1])
